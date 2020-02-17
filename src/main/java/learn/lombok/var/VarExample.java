0c2c8035c0f804cd55aeac7b2ff194012f4bd3cc
Kovalenko Nikita <nikita.kov91@gmail.com>
2020-02-17 17:25:07 +0700

    add var example

---
+++ src/main/java/learn/lombok/var/VarExample.java
@@ -0,0 +1,25 @@
+package learn.lombok.var;
+
+import java.util.ArrayList;
+
+public class VarExample {
+
+    public static String example() {
+        var example = new ArrayList<String>();
+        example.add("Hello, World!");
+
+
+        example = new ArrayList<>();
+        example.add("not final");
+        // example = new LinkedList<String>();
+        // doesn't work, type was inferred from initializer expression
+
+        var foo = example.get(0);
+        return foo.toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(example());
+    }
+
+}
